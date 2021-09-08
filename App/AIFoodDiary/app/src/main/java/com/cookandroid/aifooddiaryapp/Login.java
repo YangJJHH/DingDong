19c225adf98a1bd3448c989a01bd25b29142c720
inseonyun <yuninseon@naver.com>
2021-09-08 23:03:31 +0900

    "test_1"

--- App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/Login.java
+++ App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/Login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Login extends AppCompatActivity {//인선
+    //지금 수정하는중
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
