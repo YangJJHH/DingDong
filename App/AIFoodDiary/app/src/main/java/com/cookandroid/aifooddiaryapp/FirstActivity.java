4a9af31c9698c81895ea5623cf69340a72e9a3ef
YangJJHH <wogur2561@naver.com>
2021-09-08 22:41:39 +0900

    create first Activity

---
+++ App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/FirstActivity.java
@@ -0,0 +1,15 @@
+package com.cookandroid.aifooddiaryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
---
+++ App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/Login.java
@@ -0,0 +1,14 @@
+package com.cookandroid.aifooddiaryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
