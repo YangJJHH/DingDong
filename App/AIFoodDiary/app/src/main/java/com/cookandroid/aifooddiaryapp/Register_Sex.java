5305d3305a56b1763392e1446c43809268e731d6
inseonyun <yuninseon@naver.com>
2021-09-12 00:58:43 +0900

    "Added Register_sex and Register_body"

---
+++ App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/Register_Sex.java
@@ -0,0 +1,42 @@
+package com.cookandroid.aifooddiaryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+// !!!!!!!!!!인텐트 부분 수정해야함 !!!!!!!!!!!!!!!!!!!
+
+public class Register_Sex extends AppCompatActivity {
+    ImageButton imgbtn_man, imgbtn_woman;  // 남성, 여성 이미지 버튼 위젯 변수 선언
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_sex);
+
+        // 각 이미지버튼 변수와 위젯 id 연결
+        imgbtn_man = (ImageButton) findViewById(R.id.imgbtn_man);
+        imgbtn_woman = (ImageButton) findViewById(R.id.imgbtn_woman);
+
+        // imgbtn_man(남성 버튼)이 눌렸을 때의 이벤트 처리
+        imgbtn_man.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register_Sex.this, Register_body.class);
+                startActivity(intent);
+            }
+        });
+
+        // imgbtn_woman(여성 버튼)이 눌렸을 때의 이벤트 처리
+        imgbtn_woman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register_Sex.this, Register_body.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/Register_body.java
@@ -0,0 +1,14 @@
+package com.cookandroid.aifooddiaryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_body extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_body);
+    }
+}
