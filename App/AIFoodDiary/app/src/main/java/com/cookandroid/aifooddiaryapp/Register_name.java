f4939b77313ac9226f3f9278a04da09d9662ac56
inseonyun <yuninseon@naver.com>
2021-09-12 21:12:39 +0900

    "Add a intent"

--- App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/Register_name.java
+++ App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/Register_name.java
@@ -2,15 +2,29 @@ package com.cookandroid.aifooddiaryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+// !!!!!!!!!!인텐트 부분 수정해야함 !!!!!!!!!!!!!!!!!!!
 
 public class Register_name extends AppCompatActivity {
-
+    Button btn_next;    // 다음 버튼 위젯 변수 선언
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_name);
 
+        btn_next = (Button) findViewById(R.id.btn_next);
 
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register_name.this, Register_sex.class);
+                startActivity(intent);
+            }
+        });
     }
 }
