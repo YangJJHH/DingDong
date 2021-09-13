43d16d946b6e1eb32363c201bfcbbf203485927b
inseonyun <yuninseon@naver.com>
2021-09-13 13:39:28 +0900

    "Modify xml files"

--- App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/Register_sex.java
+++ App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/Register_sex.java
@@ -4,33 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 // !!!!!!!!!!인텐트 부분 수정해야함 !!!!!!!!!!!!!!!!!!!
 
 public class Register_sex extends AppCompatActivity {
     ImageButton imgbtn_man, imgbtn_woman;  // 남성, 여성 이미지 버튼 위젯 변수 선언
+    Button btn_next;    // 버튼 위젯 변수 선언
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_sex);
 
-        // 각 이미지버튼 변수와 위젯 id 연결
+        // 각 버튼 변수와 위젯 id 연결
         imgbtn_man = (ImageButton) findViewById(R.id.imgbtn_man);
         imgbtn_woman = (ImageButton) findViewById(R.id.imgbtn_woman);
+        btn_next = (Button) findViewById(R.id.btn_next);
 
         // imgbtn_man(남성 버튼)이 눌렸을 때의 이벤트 처리
         imgbtn_man.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Register_sex.this, Register_body.class);
-                startActivity(intent);
+                imgbtn_man.setImageResource(R.drawable.sex_man_onclick);
+                imgbtn_woman.setImageResource(R.drawable.sex_woman);
             }
         });
 
         // imgbtn_woman(여성 버튼)이 눌렸을 때의 이벤트 처리
         imgbtn_woman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgbtn_man.setImageResource(R.drawable.sex_man);
+                imgbtn_woman.setImageResource(R.drawable.sex_woman_onclick);
+            }
+        });
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Register_sex.this, Register_body.class);
