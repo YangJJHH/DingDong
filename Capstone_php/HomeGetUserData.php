277412be0dd5faea2d2273318499b24fbb400466
inseonyun <yuninseon@naver.com>
2021-11-04 19:47:37 +0900

    홈화면 프로그래스바 설정 관련 php 추가

---
+++ Capstone_php/HomeGetUserData.php
@@ -0,0 +1,25 @@
+<?php 
+    $connection = mysqli_connect("10.50.239.148", "ding", "Ding1234!", "DingDong");
+    mysqli_query($connection,'SET NAMES utf8');
+
+    $userID = $_POST["userID"];
+
+    $statement = mysqli_prepare($connection, "SELECT userHeight, userBMR, userFoodpurpose FROM USERS WHERE userID = ?");
+    mysqli_stmt_bind_param($statement, "s", $userID);
+    mysqli_stmt_execute($statement);
+
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $userHeight, $userBMR, $userFoodpurpose);
+
+    $row = array();
+
+    while(mysqli_stmt_fetch($statement)) {
+        $row["userHeight"] = $userHeight;
+        $row["userBMR"] = $userBMR;
+        $row["userFoodpurpose"] = $userFoodpurpose;
+        
+    }
+   
+    echo json_encode($row);
+
+?>
