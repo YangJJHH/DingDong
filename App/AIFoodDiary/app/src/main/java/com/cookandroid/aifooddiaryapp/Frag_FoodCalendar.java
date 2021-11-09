4dcad02fa7eafd852add46e61bcd572d170accf3
inseonyun <yuninseon@naver.com>
2021-11-09 22:27:33 +0900

    푸드 캘린더 화면 구성 및 데이트 피커 기능 구현

--- App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/Frag_FoodCalendar.java
+++ App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/Frag_FoodCalendar.java
@@ -1,21 +1,128 @@
 package com.cookandroid.aifooddiaryapp;
 
+import android.app.DatePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Frag_FoodCalendar extends Fragment {
     private View view;
+
+    // 위젯 변수 생성
+    TextView tv_date;
+    CardView cv_morning, cv_after, cv_dinner, cv_snack;
+    ImageView img_morning, img_after, img_dinner, img_snack;
+
+
+    // 날짜 변수 생성
+    Date today;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_foodcalendar, container, false);
+
+        // 위젯 변수 id 연결
+        tv_date = (TextView) view.findViewById(R.id.tv_date);
+
+        cv_morning = (CardView) view.findViewById(R.id.cv_morning);
+        cv_after = (CardView) view.findViewById(R.id.cv_after);
+        cv_dinner = (CardView) view.findViewById(R.id.cv_dinner);
+        cv_snack = (CardView) view.findViewById(R.id.cv_snack);
+
+        img_morning = (ImageView) view.findViewById(R.id.img_morning);
+        img_after = (ImageView) view.findViewById(R.id.img_after);
+        img_dinner = (ImageView) view.findViewById(R.id.img_dinner);
+        img_snack = (ImageView) view.findViewById(R.id.img_snack);
+
+        // 현재 날짜 불러와서 tv_date에 찍어줌
+        today = new Date();
+        tv_date.setText(new SimpleDateFormat("MM월 dd일").format(today));
+
+        // 데이트 피커 생성
+        Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),android.R.style.Theme_Holo_Light_Dialog_MinWidth, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String today = month + 1 + "월 " + dayOfMonth + "일";
+                tv_date.setText(today);
+            }
+        }, mYear, mMonth, mDay);
+        datePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
+        datePickerDialog.setTitle("날짜 선택");
+        datePickerDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        datePickerDialog.getDatePicker().setCalendarViewShown(false);
+
+        // 날짜를 클릭하면 데이트 피커가 나오고 날짜가 변경되게 함
+        tv_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+
+        // cv_morning 클릭 했을 때의 이벤트 처리
+        cv_morning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                Frag_Camera frag_camera = new Frag_Camera();
+                transaction.replace(R.id.main_frame, frag_camera);
+                transaction.commit();
+            }
+        });
+
+        // cv_after 클릭 했을 때의 이벤트 처리
+        cv_after.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                Frag_Camera frag_camera = new Frag_Camera();
+                transaction.replace(R.id.main_frame, frag_camera);
+                transaction.commit();
+            }
+        });
+
+        // cv_dinner 클릭 했을 때의 이벤트 처리
+        cv_dinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                Frag_Camera frag_camera = new Frag_Camera();
+                transaction.replace(R.id.main_frame, frag_camera);
+                transaction.commit();
+            }
+        });
+
+        // cv_morning 클릭 했을 때의 이벤트 처리
+        cv_snack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                Frag_Camera frag_camera = new Frag_Camera();
+                transaction.replace(R.id.main_frame, frag_camera);
+                transaction.commit();
+            }
+        });
 
         return view;
     }
