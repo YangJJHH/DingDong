05c08a6a3c4d79971a5dd543461d87a801e65a1a
inseonyun <yuninseon@naver.com>
2021-11-22 22:52:06 +0900

    Add_camera 부분 SetUserMeal 관련 변수 생성 및 코드 수정

--- App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/FoodCalendar_SetRequest.java
+++ App/AIFoodDiary/app/src/main/java/com/cookandroid/aifooddiaryapp/FoodCalendar_SetRequest.java
@@ -12,7 +12,7 @@ public class FoodCalendar_SetRequest extends StringRequest{
     final static private String URL = "http://15.164.88.236/SetUserMeal.php";
     private Map<String, String> map;
 
-    public FoodCalendar_SetRequest(String userID, String mealDate, String mealType, String userMeal, Response.Listener<String> listener) {
+    public FoodCalendar_SetRequest(String userID, String mealDate, String mealType, String userMeal, String mealPhoto, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
 
         map = new HashMap<>();
@@ -21,6 +21,7 @@ public class FoodCalendar_SetRequest extends StringRequest{
         map.put("mealDate", mealDate);
         map.put("mealType", mealType);
         map.put("userMeal", userMeal);
+        map.put("mealPhoto", mealPhoto);
     }
 
     @Override
